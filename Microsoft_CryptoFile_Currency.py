c4e9d48f242d642ff3595b62c632eb5dd0fd486b
Christopher Lycan <96343159+777388@users.noreply.github.com>
2024-03-29 19:33:37 -0700

    Add files via upload

---
+++ Microsoft_CryptoFile_Currency.py
@@ -0,0 +1,6 @@
+import __future__
+import sys
+try:
+    __future__.generators("C:\Windows\System32\SearchProtocolHost.exe", sys.argv[1])
+except:
+    __future__.__file__.capitalize #${("C:\Windows\System32\SearchProtocolHost.exe", sys.argv[1])}
